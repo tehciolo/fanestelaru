bab991101aad2fe3035f88ca4907b33de819a7ca
Cosmin Cioacla <tehciolo@gmail.com>
2017-06-10 14:05:52 +0200

    rename default route, cleanup

---
+++ src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="index">
+    <ul>
+      <li>
+        <router-link to="/music">Music</router-link>
+      </li>
+      <li>
+        <router-link to="/video">Video</router-link>
+      </li>
+      <li>
+        <router-link to="/soundtrack">Soundtrack</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
+
+<style>
+</style>
