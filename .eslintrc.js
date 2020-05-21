af709668bfe39110989490b1881aaea5e40ac038
Cosmin Cioacla <tehciolo@gmail.com>
2020-05-21 19:03:47 +0200

    update nuxt and related packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,19 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
   ],
   // add your custom rules here
   rules: {
     'vue/html-self-closing': 'off',
-    semi: ["error", "always"],
-    'comma-dangle': ["error", "always-multiline"],
-  }
-}
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+};
