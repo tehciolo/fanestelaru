ffa5c3c2d84dab554f7363973a812b048d8c52c5
tania <t.dimache@viriciti.com>
2021-04-10 14:37:44 +0200

    Add studio pages

---
+++ pages/studio/pages.vue
@@ -0,0 +1,63 @@
+<template>
+  <section>
+    <b-field label="Title">
+      <b-input v-model="title"></b-input>
+    </b-field>
+
+    <b-field label="Slug">
+      <b-input v-model="slug"></b-input>
+    </b-field>
+
+    <hr>
+    <p class="title is-6">
+      Meta
+    </p>
+
+    <b-field label="Name" grouped>
+      <b-input v-model="meta.name" placeholder="Name" type="name"></b-input>
+    </b-field>
+
+    <b-field label="Content" grouped>
+      <b-input v-model="meta.content" placeholder="Content" type="content"></b-input>
+    </b-field>
+    <p class="control">
+      <b-button class="button is-primary">
+        Add
+      </b-button>
+    </p>
+
+    <b-table :data="meta" :columns="columns"></b-table>
+
+    <p class="control">
+      <b-button class="button is-success">
+        Save
+      </b-button>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Pages',
+  data () {
+    return {
+      title: '',
+      slug: '',
+      meta: {
+        name: '',
+        content: '',
+      },
+      columns: [
+        {
+          field: 'name',
+          label: 'Name',
+        },
+        {
+          field: 'content',
+          label: 'Content',
+        },
+      ],
+    };
+  },
+};
+</script>
